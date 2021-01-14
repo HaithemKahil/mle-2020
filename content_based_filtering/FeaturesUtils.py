b2ed0a08f111f22d6f0e438ca4bfb5b3c5e2e8b0
HaithemKahil <eh_kahil@esi.dz>
2021-01-14 17:49:30 +0100

    Adding average movie rating feature

--- content_based_filtering/FeaturesUtils.py
+++ content_based_filtering/FeaturesUtils.py
@@ -11,3 +11,11 @@ def get_most_similar(similarity, movies, movie_name, year=None, top=10):
     index_movie = get_movie_id(movies, movie_name, year)
     best = similarity[index_movie].argsort()[::-1]
     return [(ind, get_movie_name(movies, ind), similarity[index_movie, ind]) for ind in best[:top] if ind != index_movie]
+
+
+def get_avg_movie_rating(movies, movie_id):
+    movie_ratings = movies[movies['movie_id'] == movie_id]
+    result = 0
+    for movie in movie_ratings:
+        result+= movie.rating
+    return result/len(movie_ratings)
